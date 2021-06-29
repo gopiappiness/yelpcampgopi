83d9b9c6bd880542696fdfb93bdcf392d6360b53
gopincc31 <gopincc31@gmail.com>
2021-06-29 15:23:13 -0700

    make it better

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const MongoDBStore = require('connect-mongo')(session);
 
 // const dbUrl=process.env.DB_URL;
 
-const dbUrl='mongodb://localhost:27017/yelp-camp';
+const dbUrl=process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 //database connection with localhost 27017
 mongoose.connect(dbUrl,{
